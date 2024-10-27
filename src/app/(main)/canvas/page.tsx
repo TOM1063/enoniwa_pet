c25909c82f26641b3061ac76612a75ab07904f91
TOM1063 <tomshina.dev@gmail.com>
2024-10-27 14:50:25 +0900

    handle p5 instance

--- src/app/(main)/canvas/page.tsx
+++ src/app/(main)/canvas/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Canvas } from "@/components/canvas/canvas";
+import { CanvasComponent } from "@/components/canvas/canvas";
 import { ArrowLeftIcon, CheckIcon, PaletteIcon } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
@@ -15,30 +15,7 @@ export default function CanvasPage() {
   };
   return (
     <div className="flex justify-center items-center h-[calc(100vh-3rem)] w-full bg-gray-100">
-      <div className="rounded-lg bg-white shadow-lg">
-        <div className="w-full h-8 bg-white rounded-t-lg shadow-md flex justify-between items-center px-4">
-          <Link href="/gallery">
-            <ArrowLeftIcon className="w-6 h-6 text-gray-500" />
-          </Link>
-          {/* <button
-              className="text-sm font-bold text-gray-500 mr-2"
-              onClick={() => setShowColorPicker(!showColorPicker)}
-            >
-              <PaletteIcon className="w-6 h-6" />
-            </button>
-            {showColorPicker && (
-              <input
-                type="color"
-                className="w-8 h-8 p-0 border-none"
-              onChange={(e) => console.log(e.target.value)} // Handle color change
-            />
-          )} */}
-          <button className="text-sm font-bold text-gray-500" onClick={() => {upload()}}>
-            <CheckIcon className="w-6 h-6" />
-          </button>
-        </div>
-        <Canvas getData={getData} />
-      </div>
+        <CanvasComponent/>
     </div>
   );
 }
--- src/components/canvas/canvas-p5.ts
+++ src/components/canvas/canvas-p5.ts
@@ -1,49 +1,94 @@
 import { P5CanvasInstance } from "react-p5-wrapper";
 import P5 from "p5";
 
-const p5Canvas = (p5: P5CanvasInstance, color: string) => {
-  let shadowLayer: P5.Graphics;
-  let lineLayer: P5.Graphics;
-  let prevMouseX: number;
-  let prevMouseY: number;
-  let picker: P5.ColorPicker;
+class Canvas {
+  shadowLayer: P5.Graphics | null;
+  lineLayer: P5.Graphics | null;
+  prevMouseX: number;
+  prevMouseY: number;
+  picker: any;
+  p5: P5;
+  canvas: any;
+  container: HTMLElement;
+  uploadDataCallback: (data: any) => void;
+  constructor( container: HTMLElement, uploadData: (data: any) => void) {
+      this.prevMouseX = 0;
+      this.prevMouseY = 0;
+      this.picker = null;
+      this.canvas = null;
+      this.shadowLayer = null;
+      this.lineLayer = null;
+      this.container = container;
+      this.p5 = new P5(this.sketch, container);
+      this.uploadDataCallback = uploadData;
+  }
+  sketch = (p5: P5) => {
+      p5.setup = () => {
+            this.setup(p5);
+      }
+      p5.draw = () => {
+            this.draw(p5);
+      }
+  }
+  setup(p5: P5) {
+      p5.createCanvas(800, 800);
+      this.shadowLayer = p5.createGraphics(800, 800);
+      this.lineLayer = p5.createGraphics(800, 800);
+      this.prevMouseX = p5.mouseX;
+      this.prevMouseY = p5.mouseY;
+      this.picker = p5.createColorPicker("deeppink");
+      this.picker.position(200, 100);
+  }
+  draw(p5: P5) {
+      if (this.shadowLayer && this.lineLayer) {
+      this.shadowLayer.clear();
+      this.lineLayer.clear();
+  
+          // 影を描画
+          this.shadowLayer.stroke(200); // 半透明の黒
+          this.shadowLayer.strokeWeight(5);
+          this.shadowLayer.line(this.prevMouseX + 14, this.prevMouseY + 14, p5.mouseX + 14, p5.mouseY + 14);
+          this.shadowLayer.filter(p5.BLUR, 5);
+  
+  
+      // 線を描画
+      this.lineLayer.stroke(this.picker.color()); // 赤色
+      this.lineLayer.strokeWeight(3);
+      this.lineLayer.line(this.prevMouseX, this.prevMouseY, p5.mouseX, p5.mouseY);
+  
+      // 影のレイヤーをメインキャンバスに描画
+      p5.image(this.shadowLayer, 0, 0);
+  
+      // 線のレイヤーをメインキャンバスに描画
+      p5.image(this.lineLayer, 0, 0);
+  
+      // 現在のマウス位置を次のフレームのために保存
+      this.prevMouseX = p5.mouseX;
+      this.prevMouseY = p5.mouseY;
+      }
+      p5.noStroke();
+      p5.fill(255, 0, 0);
+      p5.circle(100, 100, 10);
+  }
+  uploadData() {
+      this.uploadDataCallback("test");
+  }
+  destroy() {
+      if (this.canvas) {
+            this.container.removeChild(this.canvas);
+            this.canvas = null;
+      }
+      if (this.shadowLayer) {
+            this.shadowLayer.remove();
+      }
+      if (this.lineLayer) {
+            this.lineLayer.remove();
+      }
+      this.prevMouseX = 0;
+      this.prevMouseY = 0;
+      this.picker = null;
+      this.p5.remove();
+    }
+}
 
-  p5.setup = () => {
-    p5.createCanvas(800, 800);
-    shadowLayer = p5.createGraphics(800, 800);
-    lineLayer = p5.createGraphics(800, 800);
-    prevMouseX = p5.mouseX;
-    prevMouseY = p5.mouseY;
-    picker = p5.createColorPicker("deeppink");
-    picker.position(200, 100);
-  };
-
-  p5.draw = () => {
-    shadowLayer.clear();
-    lineLayer.clear();
-
-        // 影を描画
-        shadowLayer.stroke(200); // 半透明の黒
-        shadowLayer.strokeWeight(5);
-        shadowLayer.line(prevMouseX + 14, prevMouseY + 14, p5.mouseX + 14, p5.mouseY + 14);
-        shadowLayer.filter(p5.BLUR, 5);
-
-
-    // 線を描画
-    lineLayer.stroke(picker.color()); // 赤色
-    lineLayer.strokeWeight(3);
-    lineLayer.line(prevMouseX, prevMouseY, p5.mouseX, p5.mouseY);
-
-    // 影のレイヤーをメインキャンバスに描画
-    p5.image(shadowLayer, 0, 0);
-
-    // 線のレイヤーをメインキャンバスに描画
-    p5.image(lineLayer, 0, 0);
-
-    // 現在のマウス位置を次のフレームのために保存
-    prevMouseX = p5.mouseX;
-    prevMouseY = p5.mouseY;
-  };
-};
-
-export default p5Canvas;
+export {Canvas};
